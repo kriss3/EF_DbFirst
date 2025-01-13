bebac557f08ee6289b40f5085d0d3a11ba38037e
Kris <kris_s2@outlook.com>
2025-01-12 23:10:49 -0800

    kata: Following EF Core YT 5 piece video and adding some minor changes to the approach.

---
+++ ConAppContosoPizza-App/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using static System.Console;
+
+namespace ConAppContosoPizza_App;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		WriteLine("Contoso Pizza. EF Core - Code First.");
+	}
+}
