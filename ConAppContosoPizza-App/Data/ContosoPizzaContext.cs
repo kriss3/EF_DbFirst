f5524d4ade621cc17ad432590d323d3a40d0da8a
Kris <kris_s2@outlook.com>
2025-01-12 23:52:16 -0800

    kata: adding ContosoPizzaContext file to the project.

---
+++ ConAppContosoPizza-App/Data/ContosoPizzaContext.cs
@@ -0,0 +1,22 @@
+﻿using ConAppContosoPizza_App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppContosoPizza_App.Data;
+public class ContosoPizzaContext : DbContext
+{
+	public DbSet<Customer> Customers { get; set; } = null!;
+	public DbSet<Order> Orders { get; set; } = null!;
+	public DbSet<Product> Products { get; set; } = null!;
+	public DbSet<OrderDetails> OrderDetails { get; set; } = null!;
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(@"AzureConnectionString");
+	}
+}
