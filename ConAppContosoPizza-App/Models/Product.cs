1a158d0724b5c75cf26c4ef5939ed54748f9d777
Kris <kris_s2@outlook.com>
2025-01-12 23:34:32 -0800

    kata: adding Models folder and my first entity: Product.

---
+++ ConAppContosoPizza-App/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConAppContosoPizza_App.Models;
+public class Product
+{
+	public int Id { get; set; }
+	public string? Name { get; set; } = null;
+
+	[Column(TypeName = "decimal(6, 1)")]
+	public decimal Price { get; set; }
+}
