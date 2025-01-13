689c97118c8a6c70268cfbff0909db45919bb4d9
Kris <kris_s2@outlook.com>
2025-01-12 23:40:10 -0800

    kata: Adding Order and Customer entity

---
+++ ConAppContosoPizza-App/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace ConAppContosoPizza_App.Models;
+public class Customer
+{
+	public int Id { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? Address { get; set; }
+	public string? Phoned { get; set; }
+	public ICollection<Order> Orders { get; set; } = [];
+}
--- ConAppContosoPizza-App/Models/Product.cs
+++ ConAppContosoPizza-App/Models/Product.cs
@@ -4,7 +4,7 @@ namespace ConAppContosoPizza_App.Models;
 public class Product
 {
 	public int Id { get; set; }
-	public string? Name { get; set; } = null;
+	public string Name { get; set; } = null!;
 
 	[Column(TypeName = "decimal(6, 1)")]
 	public decimal Price { get; set; }
