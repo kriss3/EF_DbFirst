384826c61c69c3b366b8f2cd894eb4dc46c4195c
Kris <kris_s2@outlook.com>
2025-01-12 23:47:34 -0800

    kata: Adding Order/OrderDetails and updating Customer entity.

--- ConAppContosoPizza-App/Models/Customer.cs
+++ ConAppContosoPizza-App/Models/Customer.cs
@@ -2,9 +2,9 @@
 public class Customer
 {
 	public int Id { get; set; }
-	public string? FirstName { get; set; }
-	public string? LastName { get; set; }
+	public string FirstName { get; set; } = null!;
+	public string LastName { get; set; } = null!;	
 	public string? Address { get; set; }
 	public string? Phoned { get; set; }
-	public ICollection<Order> Orders { get; set; } = [];
+	public ICollection<Order> Orders { get; set; } = null!;
 }
--- ConAppContosoPizza-App/Models/Order.cs
+++ ConAppContosoPizza-App/Models/Order.cs
@@ -7,4 +7,11 @@ using System.Threading.Tasks;
 namespace ConAppContosoPizza_App.Models;
 public class Order
 {
+	public int Id { get; set; }
+	public DateTime OrderPlaced { get; set; }
+	public DateTime OrderFulfilled { get; set; }
+	public int CustomerId { get; set; }
+
+	public Customer Customer { get; set; } = null!;
+	public ICollection<OrderDetails> OrderDetails { get; set; } = null!;
 }
