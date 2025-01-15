7ad9ea1fb505ad29f5786cc3376856acbb7d9e9b
Kris <kris_s2@outlook.com>
2025-01-14 18:49:14 -0800

    kata: add EfOps service and AddProduct operations.

---
+++ ConAppContosoPizza-App/Services/EfOperationsService.cs
@@ -0,0 +1,20 @@
+﻿using ConAppContosoPizza_App.Data;
+using ConAppContosoPizza_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppContosoPizza_App.Services;
+public class EfOperationsService(ContosoPizzaContext context)
+{
+	private readonly ContosoPizzaContext _context = context;
+
+	public void AddProduct(Product product)
+	{
+		_context.Products.Add(product);
+		_context.SaveChanges();
+	}
+}
+
